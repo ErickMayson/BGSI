cbf0bac28db35d9536654179e23b5f33c50e009c
gLeirbag <gabriel.f_gomazako@outlook.com>
2023-11-26 11:16:53 -0300

    Lista de Entidades, e Entidades

---
+++ src/entidades/Entidade.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Entidade:
+    def __init__(self, nome, escala, posicao, corBorda = (0,0,0), corPreenchimento = (255, 255, 255)):
+        self.corBorda = corBorda
+        self.corPreenchimento = corPreenchimento
+        self.nome = nome
+        self.escala = escala
+        self.posicao = posicao
+
+class Retangulo(Entidade):
+    def __init__(self, cordsMin, cordsMax, nome, escala, posicao, corBorda = (0,0,0), corPreenchimento = (255, 255, 255), ):
+        super().__init__(nome, escala, posicao, corBorda = (0,0,0), corPreenchimento = (255, 255, 255))
+        self.cordsMin = cordsMin
+        self.cordsMax = cordsMax
+    
+    def draw(self, surf):
+        pygame.draw.rect(surf, self.corPreenchimento, (self.cordsMin, self.cordsMax))
+        pygame.display.flip()
---
+++ src/main.py
@@ -0,0 +1,39 @@
+# Importa toda a biblioteca do tk para o programa.   
+from tkinter import *
+# Importa o submódulo ttk para o programa, contem funcionalidades mais atualizadas.
+from tkinter import ttk
+
+import pygame
+
+pygameWindow = {'size': (800, 400), 'backgroundColor': (255, 255, 255), 'caption': 'Janela PyGame'}
+janelaPygame = pygame.display.set_mode(pygameWindow['size'])
+pygame.display.set_caption(pygameWindow['caption'])
+
+
+entidadesCriadas = []
+
+from entidades.Entidade import Retangulo
+
+retangulo1 = Retangulo((50, 50), (120, 200), 'retangulo1', 1, 1)
+entidadesCriadas.append(retangulo1)
+retangulo2 = Retangulo((100, 110), (154, 300), 'retangulo2', 1, 1)
+entidadesCriadas.append(retangulo2)
+
+from janelas.jGerenciadorObjetos import jGerenciadorObjetos
+
+#jGerenciadorObjetos()
+#print("Barata")
+#executar()
+#jGerenciadorObjetos()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            running = False
+    for entidade in entidadesCriadas:
+        entidade.draw(janelaPygame)
+    #jGerenciadorObjetos()
+
+pygame.quit()
+quit()
