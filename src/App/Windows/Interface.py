930af6abc708281a0339aa8b3be4a656d7dc0fc6
ErickMayson <Ferreiraerick0101@gmail.com>
2023-11-28 00:58:18 -0300

    Adicionada a funcao de mudar o tamanho da janela mas nao ta implementada

--- src/App/Windows/Interface.py
+++ src/App/Windows/Interface.py
@@ -70,6 +70,37 @@ class Interface:
         self.createObjectButton = tk.Button(master, text="Salvar", command=lambda: self.gerenciadorSINGLETON.saveDisplayFile(), bg=self.fontColor, font=self.normalFont, width=10, height=1, bd=4, relief=tk.RAISED, padx=10, pady=5)
         self.createObjectButton.grid(row=3, column=1, columnspan=2, pady=5, padx=5)
         
+    #    # Isso aqui deveria atualizar a window mas sei la
+    #    self.windowWidth = tk.Entry(self.master)
+    #    self.windowHeight = tk.Entry(self.master)
+    #    
+    #    tk.Label(self.master, text="Width:").grid(row=4, column=1, padx=5, pady=5, sticky="e")
+    #    self.windowWidth.grid(row=4, column=2, padx=5, pady=5, sticky="w")
+    #    
+    #    tk.Label(self.master, text="Height:").grid(row=5, column=1, padx=5, pady=5, sticky="e")
+    #    self.windowHeight.grid(row=5, column=2, padx=5, pady=5, sticky="w")
+    #    
+    #    # Botao :)
+    #    self.update_size_button = tk.Button(master, text="Update Size", command=self.updateSize, bg=self.fontColor, font=self.normalFont, width=10, height=1, bd=4, relief=tk.RAISED, padx=10, pady=5)
+    #    self.update_size_button.grid(row=6, column=1, columnspan=2, pady=5, padx=5)
+        
+    def updateSize(self):
+        try:
+            width = int(self.windowWidth.get())
+            height = int(self.windowHeight.get())
+            
+            # Atualiza o tamanho da janela
+            pygameWindow['size'] = (width, height)
+            
+            # Atualiza o display
+            pygame.display.set_mode(pygameWindow['size'])
+
+            # Debug
+            print(f"PygameWindow size updated to: {width} x {height}")
+            
+        except ValueError:
+            messagebox.showerror("Error", "Invalid input. Please enter valid numbers for width and height.")
+        
     
     def updateEntityList(self, entity):
         self.entityList.insert("", tk.END, values=(entity.nome, entity.__class__.__name__))
